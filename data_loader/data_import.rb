cea8cbe3383b6220b27cee2a4c526504b9bc6e4a
Jonathan Hicks <jonathan.r.hicks@gmail.com>
2010-03-28 12:46:18 -0500

    Adding more specs and a new table, wave coverage. This stores the x-intercepts from the data files.

--- data_loader/data_import.rb
+++ data_loader/data_import.rb
@@ -7,7 +7,9 @@ require 'data_loader'
 
 DataLoader.setup()
 
-dir = './data'
-Dir.glob(dir + '/*.txt') do |file_name|
+dir = './data/'
+dir = ARGV[0] if ARGV[0]
+
+Dir.glob(dir + '*.txt') do |file_name|
   DataLoader::process_file(file_name)
 end
--- data_loader/lib/data_loader.rb
+++ data_loader/lib/data_loader.rb
@@ -4,6 +4,7 @@ require 'pathname'
 
 module DataLoader
   autoload :ScopeInput, 'data_loader/scope_input'
+  autoload :WaveCoverage, 'data_loader/wave_coverage'
   
   class << self
     def setup(debug = false)
@@ -12,60 +13,87 @@ module DataLoader
       DataMapper.setup(:default, 'postgres://localhost/sea_data')
 
       ScopeInput.auto_migrate!
+      WaveCoverage.auto_migrate!
     end
 
     def process_file(file_name)
-      root = Pathname.new(file_name).basename.to_s.gsub(/.txt/, '')
-      circuit, energy, pixel, scan = root.split('_')
-      circuit_info = { 
-        :circuit => circuit, :energy => energy,
-        :pixel => pixel, :scan => scan
-      }
+      circuit_info = parse_filename(file_name)
 
       lines = File.readlines(file_name).map {|l| l.rstrip}[9..-1]
 
       max = { :voltage => 0.0, :time => 0.0 }
       min = { :voltage => 0.0, :time => 0.0 }
-      prev_volt = 0.0
+      prev_point = { :voltage => 0.0, :time => 0.0 }
       
       lines.each_with_index do |line, i|
         if i == 0
-          max, min, prev_volt = bootstrap(line, max, min)
+          max, min, prev_point = bootstrap(line, max, min)
           next
         end
         t, v = line.split(' ')
         t = t.to_f
         v = v.to_f
 
-        if v > max[:voltage]
+        if (t > 208.0 && t < 209.12) && (prev_point[:voltage] < 0.0 && v > 0.0)
+          intercept = calculate_intercept(prev_point, { :voltage => v, :time => t })
+          # 209.12 should be configurable.
+          x_intercept = 209.12 - intercept
+          WaveCoverage.create(circuit_info.merge({ :x_intercept => x_intercept, :strike_point => 209.12 }))
+        end
+
+        if v > max[:voltage] && v > 0.3
           max[:voltage] = v
           max[:time] = t
         end
 
-        if v < min[:voltage]
+        if v < min[:voltage] && v < -0.3
           min[:voltage] = v
           min[:time] = t
         end
 
-        if (prev_volt < 0.0 && v >= 0.0) || lines.size == i + 1
+        if (prev_point[:voltage] < 0.0 && v >= 0.0) || lines.size == i + 1
           ScopeInput.create(circuit_info.merge(min)) if min[:voltage] != 0.0
           min[:voltage] = min[:time] = 0.0
         end
 
-        if (prev_volt >= 0.0 && v < 0.0) || lines.size == i + 1
+        if (prev_point[:voltage] >= 0.0 && v < 0.0) || lines.size == i + 1
           ScopeInput.create(circuit_info.merge(max)) if max[:voltage] != 0.0
           max[:voltage] = max[:time] = 0.0
         end
-        prev_volt = v
+        prev_point[:voltage] = v
+        prev_point[:time] = t
       end
     end
-    
+
+    def parse_filename(file_name)
+      root = Pathname.new(file_name).basename.to_s.gsub(/.txt/, '')
+      tokens = root.split('_')
+
+      case tokens.size
+        when 4
+          { :circuit => tokens[0], :energy => tokens[1],
+            :pixel => tokens[2], :scan => tokens[3] }
+        when 5
+          { :circuit => tokens[0], :energy => tokens[1],
+            :pixel => tokens[3], :scan => tokens[4] }
+        else
+          { }
+      end
+    end
+
     def bootstrap(line, max, min)
       t, v = line.split(' ')
       v = v.to_f
+      t = t.to_f
       max[:voltage] = v if v >= 0.0
       min[:voltage] = v if v < 0.0
-      return max, min, v
+      return max, min, { :voltage => v, :time => t }
+    end
+
+    def calculate_intercept(point1, point2)
+      slope = (point2[:voltage] - point1[:voltage]) / (point2[:time] - point1[:time])
+      b = point2[:voltage] - point2[:time] * slope
+      return -b / slope
     end
   end
 end
